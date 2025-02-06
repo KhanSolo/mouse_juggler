fbad67071de617e9c8730fbd529b818fd7045759
Konstantin <drpdrp@mail.ru>
2025-02-07 01:23:46 +0300

    ADD: error handling

--- src/main.rs
+++ src/main.rs
@@ -3,36 +3,75 @@ use std::time::Duration;
 use winapi::um::winuser::{GetCursorPos, SetCursorPos, GetSystemMetrics, SM_CXSCREEN, SM_CYSCREEN};
 use winapi::shared::windef::POINT;
 
-fn move_mouse(x: i32, y: i32) {
-    unsafe { SetCursorPos(x, y); }
+fn move_mouse(x: i32, y: i32) -> Result<(), String> {
+    unsafe {
+        if SetCursorPos(x, y) == 0 {
+            Err("SetCursorPos failed".into())
+        } else {
+            Ok(())
+        }
+    }
 }
 
-fn get_mouse_pos() -> (i32, i32) {
-    let mut point = POINT { x: 0, y: 0 };
-    unsafe { GetCursorPos(&mut point as *mut POINT) };
-    (point.x, point.y)
+fn get_mouse_pos() -> Result<(i32, i32), String> {    
+    unsafe { 
+        let mut point = POINT { x: 0, y: 0 };
+        if GetCursorPos(&mut point) == 0 {
+            Err("GetCursorPos failed".into())
+        } else {
+            Ok((point.x, point.y))
+        }
+    }
 }
 
-fn get_screen_resolution() -> (i32, i32) {
-    unsafe { (GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN)) }
+fn get_screen_resolution() -> Result<(i32, i32), String> {
+    unsafe { 
+        let width = GetSystemMetrics(SM_CXSCREEN);
+        let height = GetSystemMetrics(SM_CYSCREEN);
+        if width == 0 || height == 0 {
+            Err("GetSystemMetrics failed".into())
+        } else {
+            Ok((width, height))
+        }
+    }
+}
+
+fn handle_mouse_coords(x:i32, y:i32, zig:bool, width:i32, height:i32) {
+    let mut shift = 10;
+    if zig { shift *= -1; }
+
+    let x = (x + shift) % width;
+    let y = (y + shift) % height;
+
+    match move_mouse(x, y) {
+        Ok(()) => println!("Moved mouse to: ({}, {}), {}", x, y, if zig { "zig" } else { "zag" }),
+        Err(s) => println!("{}", s),
+    }        
+
+    thread::sleep(Duration::from_secs(5));
 }
 
 fn main() {
-    let (width, height) = get_screen_resolution();    
-    let mut zig = true;
-    loop {
-        let(mut x, mut y) = get_mouse_pos();
 
-        let mut shift = 10;
-        if zig { shift *= -1; }
-        zig = !zig;
-
-        x = (x + shift) % width;
-        y = (y + shift) % height;
-
-        move_mouse(x, y);
-        println!("Moved mouse to: ({}, {}), zig: {}", x, y, zig);
-
-        thread::sleep(Duration::from_secs(5));
+    match get_screen_resolution() {
+        Ok((width, height)) => {
+            let mut zig = true;
+            loop {
+                match get_mouse_pos() {
+                    Ok((x,y)) => {
+                        handle_mouse_coords(x, y, zig, width, height);
+                        zig = !zig;
+                    },
+                    Err(e) => {
+                        println!("{}", e);
+                        return;
+                    },
+                }
+            }
+        },
+        Err(e) => {
+            println!("{}", e);
+            return;
+        },
     }
 }
